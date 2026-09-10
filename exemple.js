5cd792b11b62b06c0a0962093c9a58e6ce340ba0
LucasBonii <158228644+LucasBonii@users.noreply.github.com>
2026-09-10 15:42:41 -0300

    Read Function

--- exemple.js
+++ exemple.js
@@ -1,8 +1,18 @@
-const income = 1250.85
+import readline from 'readline/promises'
+
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+const income = Number(await rl.question("How much is your income?"))
 const expense = 562.99
 
 const profit =  income - expense
 //console.log(`Profit: ${income-expense}`)
 
-const profitFormatado = profit.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+const profitFormatado = profit.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
 console.log(`Profit: ${profitFormatado}`)
+
+rl.close()
